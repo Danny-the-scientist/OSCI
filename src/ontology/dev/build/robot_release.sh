54cd78e74297d978ca13aa3aff5cee9d3a9fd24a
Bill Duncan <wdduncan@gmail.com>
2018-06-01 23:49:50 -0400

    create OCSI release 2018-06-01T23:49:50

--- src/ontology/dev/build/robot_release.sh
+++ src/ontology/dev/build/robot_release.sh
@@ -5,18 +5,26 @@ cp ../imports/OBIB_import.owl .
 cp ../imports/OGG_import.owl .
 cp ../imports/OMRSE_import.owl .
 
-robot \
-  merge robot merge --inputs "*.owl" --output osci-merged.owl
+robot merge \
+  --inputs "*.owl"\
+  --include-annotations true \
+  --output osci-merged.owl
    
-robot annotate --input osci-merged.owl \
+robot annotate \
+  --input osci-merged.owl \
   --ontology-iri "http://purl.obolibrary.org/obo/osci.owl" \
   --version-iri "http://purl.obolibrary.org/obo/`date '+%Y-%m-%d'`/osci.owl" \
   --output osci-annotated.owl
 
-robot \
-  reduce --input osci-annotated.owl --output osci-reduced.owl
+robot reason \
+	--input osci-annotated.owl \
+  --reasoner HermiT \
+  --annotate-inferred-axioms true \
+  --output osci.owl
+
+rm *import.owl
   
 # robot \
 #   merge --input osci-annotated.owl \
-#   reason --reasoner ELK \
+#   reason --reasoner HermiT \
 #   --output osci-reasoned.owl
